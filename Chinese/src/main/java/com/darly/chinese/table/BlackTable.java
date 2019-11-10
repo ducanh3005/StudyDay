da57298c5770088cc161d6332dfe44675e80c31f
Darly.Fronch <darlyyuhui@hotmail.com>
2019-11-11 07:44:00 +0800

    yuhui
    {
    增加表格
    
    }

---
+++ Chinese/src/main/java/com/darly/chinese/table/BlackTable.java
@@ -0,0 +1,53 @@
+package com.darly.chinese.table;
+
+public class BlackTable {
+    private int id;
+    private String  key0;
+    private String  key15;
+    private String  key30;
+    private String  key45;
+
+    public BlackTable(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKey0() {
+        return key0;
+    }
+
+    public void setKey0(String key0) {
+        this.key0 = key0;
+    }
+
+    public String getKey15() {
+        return key15;
+    }
+
+    public void setKey15(String key15) {
+        this.key15 = key15;
+    }
+
+    public String getKey30() {
+        return key30;
+    }
+
+    public void setKey30(String key30) {
+        this.key30 = key30;
+    }
+
+    public String getKey45() {
+        return key45;
+    }
+
+    public void setKey45(String key45) {
+        this.key45 = key45;
+    }
+}
--- app/src/main/java/com/darly/std/GuideActivity.java
+++ app/src/main/java/com/darly/std/GuideActivity.java
@@ -61,7 +61,7 @@ public class GuideActivity extends BaseActivity<ActivityGuideBinding, GuideViewM
     @Override
     public void initObservableView() {
         ParseJsonController.getInstance().initParseJson(this);
-
+        startActivity(new Intent(GuideActivity.this, MainActivity.class));
     }
 
     @Override
@@ -70,7 +70,7 @@ public class GuideActivity extends BaseActivity<ActivityGuideBinding, GuideViewM
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(GuideActivity.this, OpenGlActivity.class));
+
                 finish();
             }
         }, 1000);
