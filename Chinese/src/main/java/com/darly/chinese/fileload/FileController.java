96eb2fbc882d4a77970ac96174f246c06a563d6d
Zhangyuhui <zhangyh@xiaoshouyi.com>
2020-01-07 16:25:42 +0800

    feat: 调整assets文件拷贝到文件夹。调整解压。调整读取文件

---
+++ Chinese/src/main/java/com/darly/chinese/fileload/FileController.java
@@ -0,0 +1,9 @@
+package com.darly.chinese.fileload;
+
+/**
+ * author:zhangyuhui
+ * date:2020-01-0709:32
+ * description:
+ */
+public class FileController {
+}
---
+++ Chinese/src/main/java/com/darly/chinese/fileload/OnCopyFileListener.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.darly.chinese.parse;
+
+/**
+ * Description TODO:任务回调类
+ * Package com.darly.dlcommon.parse
+ * @author zhangyuhui
+ * Date 2019/7/31 17:39
+ * Company 山东新北洋信息技术股份有限公司西安分公司
+ * EMail zhangyuhui@newbeiyang.com
+ */
+public interface OnParseJsonListener {
+    /**
+     * 任务开始
+     * @param type 任务名称
+     */
+    void onStart(String type);
+
+    /**
+     * 任务完成
+     * @param type 任务名称
+     * @param ob 任务返回数据
+     */
+    void onComplete(String type,Object ob);
+
+    /**任务加载进度
+     * @param percent  进度值
+     */
+    void onProgress(int percent);
+    /**辅助加载进度
+     * @param percent  进度值
+     */
+    void onSecProgress(int percent);
+    /**
+     * 任务失败
+     * @param type 任务名称
+     * @param msg 失败信息
+     */
+    void onFailed(String type,String msg);
+}
